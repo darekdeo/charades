c977abefc4677601a290b8ad6ad2d888a78278f6
Darek Deoniziak <darekdeoniziak@gmail.com>
2016-11-17 10:42:01 +0100

    removed dagger

--- app/src/main/java/com/dariuszdeoniziak/charades/App.java
+++ app/src/main/java/com/dariuszdeoniziak/charades/App.java
@@ -2,34 +2,15 @@ package com.dariuszdeoniziak.charades;
 
 import android.app.Application;
 
-import com.dariuszdeoniziak.charades.components.AppComponent;
-import com.dariuszdeoniziak.charades.components.DaggerAppComponent;
-import com.dariuszdeoniziak.charades.modules.AppModule;
-import com.dariuszdeoniziak.charades.modules.DbModule;
 import com.orm.SugarContext;
 
 public class App extends Application {
-
-    protected AppComponent mAppComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         SugarContext.init(this);
-
-        initComponents();
-    }
-
-    void initComponents() {
-        mAppComponent = DaggerAppComponent.builder()
-                .appModule(new AppModule(this))
-                .dbModule(new DbModule())
-                .build();
-    }
-
-    public AppComponent getAppComponent() {
-        return mAppComponent;
     }
 
     @Override
--- app/src/main/java/com/dariuszdeoniziak/charades/activities/BaseActivity.java
+++ app/src/main/java/com/dariuszdeoniziak/charades/activities/BaseActivity.java
@@ -1,18 +1,13 @@
 package com.dariuszdeoniziak.charades.activities;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
-
-import com.dariuszdeoniziak.charades.App;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-import javax.inject.Inject;
 
 import butterknife.ButterKnife;
 
@@ -21,9 +16,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     @LayoutRes
     protected int layoutResId;
 
-    @Inject
-    protected SharedPreferences mSharedPreferences;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +23,6 @@ public abstract class BaseActivity extends AppCompatActivity {
         getAnnotations();
         setContentView(layoutResId);
         ButterKnife.bind(this);
-
-        ((App) getApplication()).getAppComponent().inject(this);
     }
 
     protected void getAnnotations() {
