594f84e748333b067a56949d5bbd1bbc168a1a7d
Darek Deoniziak <darekdeoniziak@gmail.com>
2016-12-08 09:20:59 +0100

    removed author comment from view interface

--- app/src/main/java/com/dariuszdeoniziak/charades/activities/views/CategoriesActivityView.java
+++ app/src/main/java/com/dariuszdeoniziak/charades/activities/views/CategoriesActivityView.java
@@ -1,9 +1,5 @@
 package com.dariuszdeoniziak.charades.activities.views;
 
-/**
- * Created by darek on 08.12.16.
- */
-
 public interface CategoriesActivityView {
     public void showToast(String text);
 }
