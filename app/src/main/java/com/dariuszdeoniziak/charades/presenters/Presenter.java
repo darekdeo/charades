b99aaeb7fa8bb7112c0b22eab6a0f899137e7b97
Darek Deoniziak <darekdeoniziak@gmail.com>
2017-03-28 21:36:10 +0200

    improved presenter interface

--- app/src/main/java/com/dariuszdeoniziak/charades/presenters/Presenter.java
+++ app/src/main/java/com/dariuszdeoniziak/charades/presenters/Presenter.java
@@ -2,7 +2,7 @@ package com.dariuszdeoniziak.charades.presenters;
 
 public interface Presenter<V> {
 
-    public abstract void onSave(); // called during on saveInstanceState
-    public abstract void onTakeView(V view); // called during onResume
-    public abstract void onDropView(); // called during onDestroy
+    void onSave(); // called during on saveInstanceState
+    void onTakeView(V view); // called during onResume
+    void onDropView(); // called during onDestroy
 }
