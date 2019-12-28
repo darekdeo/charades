6291a23bda4d0adcea21c60c982fae6725b0bb9b
Darek Deoniziak <darekdeoniziak@gmail.com>
2019-12-28 23:08:24 +0100

    Removed deprecated ActivityTestRule in favor of ActivityScenario

--- app/src/androidTest/java/com/dariuszdeoniziak/charades/steps/CategoriesListSteps.java
+++ app/src/androidTest/java/com/dariuszdeoniziak/charades/steps/CategoriesListSteps.java
@@ -2,8 +2,7 @@ package com.dariuszdeoniziak.charades.steps;
 
 import com.dariuszdeoniziak.charades.test.CategoriesListRobot;
 
-import org.junit.Assert;
-
+import cucumber.api.java.After;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -11,9 +10,13 @@ public class CategoriesListSteps {
 
     private CategoriesListRobot robot = new CategoriesListRobot();
 
+    @After
+    public void after() {
+        robot.close();
+    }
+
     @When("I start the categories list screen")
     public void iStartTheCategoriesListScreen() {
-        Assert.assertNotNull(robot);
         robot.launchCategoriesActivity();
     }
 
--- app/src/androidTest/java/com/dariuszdeoniziak/charades/test/CategoriesListRobot.java
+++ app/src/androidTest/java/com/dariuszdeoniziak/charades/test/CategoriesListRobot.java
@@ -3,10 +3,8 @@ package com.dariuszdeoniziak.charades.test;
 import com.dariuszdeoniziak.charades.R;
 import com.dariuszdeoniziak.charades.views.activities.CategoriesActivity;
 
-import org.junit.Rule;
-
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
-import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -14,15 +12,18 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class CategoriesListRobot {
 
-    @Rule
-    public ActivityTestRule<CategoriesActivity> testRule = new ActivityTestRule<>(CategoriesActivity.class, false, false);
+    private ActivityScenario<CategoriesActivity> scenario;
 
     public void launchCategoriesActivity() {
-        testRule.launchActivity(null);
+        scenario = ActivityScenario.launch(CategoriesActivity.class);
     }
 
     public void verifyHeaderTextIs(String text) {
         Espresso.onView(withId(R.id.categories_title))
                 .check(matches(withText(text)));
+    }
+
+    public void close() {
+        scenario.close();
     }
 }
